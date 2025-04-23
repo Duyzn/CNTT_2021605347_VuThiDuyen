4e408a0cb35eba7f30cfd47f9aa65fddeb6db848
Duyzn <vuduyen669@gmail.com>
2025-04-23 19:14:56 +0700

    Du an goc

---
+++ client/src/utils/axiosConfig.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:5000';
+
+const axiosClient = axios.create({
+    baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosClient;
